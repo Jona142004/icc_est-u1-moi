902a26d2908b01dc53f4ce40acfab229a1595db8
Jonnathan Parraga <jonnathanhola@gmail.com>
2024-09-27 13:11:49 -0500

    Actualizacion

--- src/App.java
+++ src/App.java
@@ -1,11 +1,22 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    /*public static void main(String[] args) throws Exception {
         System.out.println("Metodo de ordenemiento");
-        
-        metodosordenamiento mO = new metodosordenamiento();
+        metodosordenamiento mO = new metodosordenamiento();*/
+    public static void main(String[] args) throws Exception{
+        MetodosOrdenamiento  mO = new MetodosOrdenamiento();
         int[] arreglo = {5,7,30,12,9};
-        int[] arregloOrdenado = mO.sortBySelection(arreglo);
-        mO.printArregloOrdenado(arregloOrdenado);
+        mO.sortBySelection(arreglo,true);
+        mO.printArreglo(arreglo);
+        System.out.println();
+        mO.sortBySelection(arreglo,false);
+        mO.printArreglo(arreglo);
     }
 
+
+       /*  int[] arreglo = {5,7,30,12,9};
+        int[] arregloOrdenado = mO.sortBySelection(arreglo);
+        mO.printArregloOrdenado(arregloOrdenado);*/
+       
 }
+
+
--- src/MetodosOrdenamiento.java
+++ src/MetodosOrdenamiento.java
@@ -1,11 +1,24 @@
-public class metodosordenamiento {
+public class MetodosOrdenamiento {
 
-    public int[] sortBySelection(int[] arreglo){
+    public void sortBySelection(int[] arreglo,boolean asc){
         int tam = arreglo.length;
         
         //recorrer arreglo
-        for(int  i = 0; i < tam; i++ ){
-
+        // metodo con boolean
+        for(int i =0 ; i < tam -1 ; i++){
+            int indice = i;
+            for(int  j = i+1; j < tam  ; j++ ){
+                if (asc? arreglo[j] < arreglo[indice] : arreglo[j] > arreglo[indice]){
+                    indice=j;
+                   
+        }
+            int aux = arreglo[indice];
+            arreglo[indice] = arreglo[i];
+            arreglo[i] = aux;
+        
+            }
+            /* 
+            for(int  i = 0; i < tam; i++ ){
             int indice = i;
             for(int  j = i + 1; j < tam; j++ ){
                 if(arreglo[j] < arreglo[indice]){
@@ -18,10 +31,15 @@ public class metodosordenamiento {
             arreglo[i] = aux;
         }
         return arreglo;
-    }
-    public void printArregloOrdenado(int[]arreglo){
-        for (int elemento : arreglo){ //for each el arreglo va instanciando cada elemento del arreglo se va almacenando
-            System.out.print(elemento + "-");
+    */
         }
-}
+    }
+
+
+
+    public void printArreglo(int[]arreglo){
+        for (int i : arreglo){ //for each el arreglo va instanciando cada elemento del arreglo se va almacenando
+            System.out.print(i + "-");
+        }
+    }
 }
